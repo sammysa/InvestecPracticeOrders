096f71e340623c2c742be102b2db273101536c63
Sammy Phatlane <phatlanems@gmail.com>
2024-06-05 11:14:43 +0200

    Initial Commit

---
+++ src/main/java/za/co/sammysa/ordersApp/OrdersAppApplication.java
@@ -0,0 +1,13 @@
+package za.co.sammysa.ordersApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/za/co/sammysa/ordersApp/OrdersAppApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.sammysa.ordersApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
